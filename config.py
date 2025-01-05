bb6284c09e53c10248f7d4f3dcc4fe213109f40a
ankitcs03 <ankit.cs.03@gmail.com>
2025-01-05 16:46:06 +0530

    fix

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///inventory.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret')
-    USER_NAME = os.getenv('USER_NAME', 'rootxx')
-    USER_PASSWORD = os.getenv('USER_PASSWORD', 'rootpasswordxx')  
+    USER_NAME = os.getenv('USER_NAME', 'root')
+    USER_PASSWORD = os.getenv('USER_PASSWORD', 'rootpassword')  
